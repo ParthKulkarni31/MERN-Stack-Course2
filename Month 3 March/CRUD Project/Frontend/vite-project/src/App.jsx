0787146bf5b35c90fafd0321ce2a6eb69673e9e8
ParthKulkarni31 <parthskulkarni311005@gmail.com>
2026-04-23 13:47:00 +0530

    All CRUD done in New laptop untill the last API and all folder structures of FE and BE.

---
+++ Month 3 March/CRUD Project/Frontend/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HomePage from './screens/HomePage'
+
+const App = () => {
+
+const name = "Parth"
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <HomePage/>
+    </div>
+  )
+}
+
+export default App
